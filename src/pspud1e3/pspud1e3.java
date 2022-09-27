596d30f5f4703260631a310901c65b303106e320
damm <damm@192.168.208.170>
2022-09-27 09:44:29 +0200

    clase fantasma arreglada

--- src/pspud1e3/pspud1e3.java
+++ src/pspud1e3/pspud1e3.java
@@ -4,24 +4,25 @@
  */
 package pspud1e3;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
  * @author damm
  */
-public class Pspsud1e3 {
+public class pspud1e3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
         try {
             Process proceso = new ProcessBuilder(
-                    "C:\\Users\\Lopema\\AppData\\Local\\Programs\\Python\\Python310\\python.exe",
-                    "D:\\PSP\\pspud1e3\\src\\pspud1e3\\proceso_python.py").start();
+                    "/usr/local/bin/python3",
+                    "/Users/damm/NetBeansProjects/Pspud1e1/src/pspud1e3/proceso_python.py").start();
             BufferedReader br = new BufferedReader(
                     new InputStreamReader(proceso.getInputStream()));
             proceso.waitFor();
@@ -33,5 +34,5 @@ public class Pspsud1e3 {
             e.printStackTrace();
         }
     }
-
+    
 }
