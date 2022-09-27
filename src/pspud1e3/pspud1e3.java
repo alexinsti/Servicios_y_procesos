41ed4c7fffa6e0c8e0e20b699f8a37873ba60ffd
damm <damm@192.168.208.170>
2022-09-27 10:47:51 +0200

    Buscador de palabra en salida

--- src/pspud1e3/pspud1e3.java
+++ src/pspud1e3/pspud1e3.java
@@ -7,6 +7,8 @@ package pspud1e3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -19,6 +21,8 @@ public class pspud1e3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int contador=0;
+        String linea;
         try {
             Process proceso = new ProcessBuilder(
                     "/usr/local/bin/python3",
@@ -28,7 +32,24 @@ public class pspud1e3 {
             proceso.waitFor();
 
             int exitStatus = proceso.exitValue();
-            System.out.println("Retorno:" + br.readLine());
+            
+            linea= br.readLine();
+            String[] lineaSeparada=linea.split(" ");
+            System.out.println(linea);
+            for(String token : lineaSeparada){
+                System.out.println(token);
+                System.out.println("....");
+                
+                Pattern patron = Pattern.compile("Nombre+", Pattern.CASE_INSENSITIVE);
+                Matcher buscador = patron.matcher(token);
+                //contador++;
+                //System.out.println(contador);
+                if(buscador.find()){
+                contador++;
+                }
+            }
+            System.out.println(contador);
+            //System.out.println("Retorno:" + br.readLine());
             System.out.println("Valor de la salida:" + exitStatus);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
